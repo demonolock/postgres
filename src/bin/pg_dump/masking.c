e99be486a736088b3614affeb3ed5ef7ce510838
Viktoria Shepard <jawektor37@gmail.com>
2022-08-03 01:11:03 +0500

    [Code4All][pg_dump] add data masking option

---
+++ src/bin/pg_dump/masking.c
@@ -0,0 +1,12 @@
+/*-------------------------------------------------------------------------
+ *
+ * Data masking tool
+ *
+ *
+ * Portions Copyright (c) 1996-2022, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * src/bin/pg_dump/masking.c
+ *
+ *-------------------------------------------------------------------------
+ */
